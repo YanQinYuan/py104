c6215be214a19000cfc262ffb246cb90736cadea
yangqinyuan <791005458@qq.com>
2018-01-12 18:08:44 +0800

    add ch4

--- Chap3/project/getWeather.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-import requests
-from constant import API,LANGUAGE,KEY,UNIT
-class get_weather(object):
-	def __init__(self,api):
-        self.api = API
-        self.language = LANGUAGE
-        self.key = KEY
-        self.unit = UNIT
